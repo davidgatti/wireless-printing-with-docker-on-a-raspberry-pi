092673deccef835432a984112840c97991da8fd6
David Gatti <office@gatti.pl>
2017-02-27 21:46:32 +0100

    Background run

--- cups/start.sh
+++ cups/start.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 if [ $(grep -ci $CUPS_USER_ADMIN /etc/shadow) -eq 0 ]; then
-    useradd $CUPS_USER_ADMIN --system -G root,lpadmin --no-create-home --password $(mkpasswd $CUPS_USER_PASSWORD)
+    useradd $CUPS_USER_ADMIN --system -G root,lpadmin --no-create-home --password $CUPS_USER_PASSWORD
 fi
 
-exec /usr/sbin/cupsd -fw
+exec service cups start
